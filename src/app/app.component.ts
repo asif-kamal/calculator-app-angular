412f5d7c38647a266e75050419892f9573e94ed4
asif-kamal <asif.kamal1489@gmail.com>
2025-09-05 10:46:54 -0500

    using a service for cross-component communication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,29 +19,4 @@ export class AppComponent {
     totalInterest: number;
     totalAmountInvested: number;
   }[];
-
-  onCalculateInvestmentResults(data: InvestmentInput) {
-    const { initialInvestment, annualInvestment, expectedReturn, duration } =
-      data;
-    const annualData = [];
-    let investmentValue = initialInvestment;
-
-    for (let i = 0; i < duration; i++) {
-      const year = i + 1;
-      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-      investmentValue += interestEarnedInYear + annualInvestment;
-      const totalInterest =
-        investmentValue - annualInvestment * year - initialInvestment;
-      annualData.push({
-        year: year,
-        interest: interestEarnedInYear,
-        valueEndOfYear: investmentValue,
-        annualInvestment: annualInvestment,
-        totalInterest: totalInterest,
-        totalAmountInvested: initialInvestment + annualInvestment * year,
-      });
-    }
-
-    this.resultsData = annualData;
-  }
 }
